78b5b85b6fef1ba00642519e28a4430861f0e4fe
Franck Evina <franck.evinadoun@outlook.fr>
2026-02-10 15:06:10 +0100

    Changement plus naturelle du server.js v2

--- project-root/server.js
+++ project-root/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const hostname = 'localhost'
 app.get('/', (req, res) => {
   res.json({ message: 'API Express opérationnelle' });
 });
