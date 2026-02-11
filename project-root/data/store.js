b1159b37398976ef2bfcc2e8cc432e7ad9af2e27
bloxyj <108661344+bloxyj@users.noreply.github.com>
2026-02-11 17:39:05 +0100

    Revert "Spirezon"

--- project-root/data/store.js
+++
@@ -1,8 +0,0 @@
-let gameState = {
-    playerHP: 100,
-    enemyHP: 100,
-    playerName: "Frisk",
-    enemyName: "Sans"
-};
-
-module.exports = gameState;
--- project-root/services/player.service.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    getAttackPower: () => {
-        return Math.floor(Math.random() * 15) + 10;
-    },
-    getDefenseMultiplier: () => 0.5 
-};
