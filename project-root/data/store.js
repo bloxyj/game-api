b70f3dc808b5d5fc4bc309c80437720dc2143415
bloxyj <108661344+bloxyj@users.noreply.github.com>
2026-02-15 15:24:11 +0100

    Fix attribute casing for monsters in store.js
    Corrected casing for monster attributes in store.js.

--- project-root/data/store.js
+++ project-root/data/store.js
@@ -3,8 +3,8 @@ const store = {
     games: [],  
     players: [], 
     monsters: [
-        { id: "m1", name: "Wind Feary", hp: 50, atk: 10, xp: 10 },
-        { id: "m2", name: "Sans (Boss)", hp: 150, atk: 42, xp: 50 }
+        { id: "m1", name: "Wind Fairy", HP: 50, ATK: 10, XP: 10 },
+        { id: "m2", name: "Sans (Boss)", HP: 150, ATK: 42, XP: 50 }
     ]
 };
 
